b819cefda5322446fdbbe2bfbde1540c2ad33be1
Ruslan <ruslan.mirets@gmail.com>
2022-09-29 12:19:48 +0300

    just commit

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CityList from './components/city-list/CityList.vue';
+</script>
 
 <template>
 	<h1>Hello</h1>
+	<CityList />
 </template>
 
 <style></style>
---
+++ client/src/components/city-list/CityList.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import axios from 'axios';
+
+const fetchCities = async () => {
+	try {
+		const response = await axios.get('http://localhost:5000/api/city');
+		console.log(response.data);
+	} catch (error) {
+		console.log(error);
+	}
+};
+</script>
+
+<template>
+	<table>
+		<tr>
+			<th>Дата</th>
+			<th>Название</th>
+			<th>Количество</th>
+			<th>Расстояние</th>
+		</tr>
+		<tr>
+			<td>
+				<button @click="fetchCities">Get cities</button>
+			</td>
+		</tr>
+	</table>
+</template>
